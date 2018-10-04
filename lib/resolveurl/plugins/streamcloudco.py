d0d2eed30c95c4940ba04c41b2c61393944e4c8d
mbebe <drazah@o2.pl>
2018-10-04 14:02:46 +0200

    Update streamcloudco.py

--- lib/resolveurl/plugins/streamcloudco.py
+++ lib/resolveurl/plugins/streamcloudco.py
@@ -1,6 +1,6 @@
 """
 streamcloud.co resolveurl plugin
-Copyright (C) 2016 xxxxxx
+Copyright (C) 2018 xxxxxx
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
