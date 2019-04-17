dd41bf27e3d5a2ad7a1eef37fb84cf9a1ddeebc8
jewbmx <42426698+jewbmx@users.noreply.github.com>
2019-04-17 13:31:27 -0500

    Update vidtodo.py

--- lib/resolveurl/plugins/vidtodo.py
+++ lib/resolveurl/plugins/vidtodo.py
@@ -1,6 +1,17 @@
-# -*- coding: UTF-8 -*-
-# Vidtodo resolver for ResolveURL
-#
+"""
+    Kodi resolveurl plugin
+    Copyright (C) 2019  script.module.resolveurl
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 # Updates:
 # - 2019 02 11
  
