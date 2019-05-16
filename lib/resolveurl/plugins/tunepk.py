0f377c87f2aa34ed5ea562a6514e77775d61c7d8
jewbmx <42426698+jewbmx@users.noreply.github.com>
2019-05-16 17:16:36 -0500

    Update tunepk.py

--- lib/resolveurl/plugins/tunepk.py
+++ lib/resolveurl/plugins/tunepk.py
@@ -2,35 +2,35 @@
 tunepk resolveurl plugin
 Copyright (C) 2013 icharania
 updated Copyright (C) 2017 gujal
-updated Copyright (C) 2019 cache
-
+updated Copyright (C) 2019 cache-sk
+ 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
+ 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-
+ 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
-
+ 
 import json, time, hashlib, base64
 from lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
-
+ 
 class TunePkResolver(ResolveUrl):
     name = "tune.pk"
     domains = ["tune.pk", "tune.video"]
     pattern = '(?://|\.)(tune\.(?:video|pk))/(?:player|video|play)/(?:[\w\.\?]+=)?(\d+)'
-
+ 
     def __init__(self):
         self.net = common.Net()
-
+ 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         apiurl = 'https://api.tune.pk/v3/videos/{}'.format(media_id)
@@ -54,9 +54,9 @@ class TunePkResolver(ResolveUrl):
                     sources.append((vids[key]['label'], vids[key]['file']))
                 
                 sources.reverse() #because it is from worst to best?
-
+ 
                 video_url = helpers.pick_source(sources)
-
+ 
                 # hash recount
                 serverTime = long(jdata['timestamp']) + (int(time.time()) - int(currentTime))
                 hashLifeDuration = long(jdata['data']['duration']) * 5
@@ -72,38 +72,17 @@ class TunePkResolver(ResolveUrl):
                         pathUrl = video_url[startOfPathUrl:None]
                     except ValueError:
                             raise ResolverError('This video cannot be played.')
-
+ 
                 htoken = hashlib.md5(str(expiryTime) + pathUrl + ' ' + 'c@ntr@lw3biutun3cb').digest()
                 htoken = base64.urlsafe_b64encode(htoken).replace('=', '').replace('\n', '')
                 video_url = video_url + '?h=' + htoken + '&ttl=' + str(expiryTime)
-
+ 
                 headers = {'Referer': web_url,
                            'User-Agent': common.FF_USER_AGENT}
-
+ 
                 return video_url + helpers.append_headers(headers)
         except:
             raise ResolverError('This video has been removed due to a copyright claim.')
         
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://tune.pk/video/{media_id}/')
-
-    @classmethod
-    def get_settings_xml(cls):
-        xml = super(cls, cls).get_settings_xml()
-        xml.append('<setting label="Video Quality" id="%s_quality" type="enum" values="High|Medium|Low" default="0" />' % (cls.__name__))
-        return xml
-                    sources.append((vids[key]['label'], vids[key]['file']))
-                headers = {'Referer': web_url,
-                           'User-Agent': common.FF_USER_AGENT}
-                return helpers.pick_source(sources) + helpers.append_headers(headers)
-        except:
-            raise ResolverError('This video has been removed due to a copyright claim.')
-        
-    def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://tune.pk/video/{media_id}/')
-
-    @classmethod
-    def get_settings_xml(cls):
-        xml = super(cls, cls).get_settings_xml()
-        xml.append('<setting label="Video Quality" id="%s_quality" type="enum" values="High|Medium|Low" default="0" />' % (cls.__name__))
-        return xml
