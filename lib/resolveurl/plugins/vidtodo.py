3b78f44b84bcaa681d872f0a02952b4ded94b83b
jewbmx <42426698+jewbmx@users.noreply.github.com>
2019-07-23 08:59:57 -0500

    Update vidtodo.py
    swapped the {host} bit out for the working domain to make it work again.
    not sure if theres a better way todo this.

--- lib/resolveurl/plugins/vidtodo.py
+++ lib/resolveurl/plugins/vidtodo.py
@@ -65,4 +65,4 @@ class VidToDoResolver(ResolveUrl):
         raise ResolverError('Unable to locate video')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
+        return self._default_get_url(host, media_id, template='https://vidtodu.com/{media_id}')
