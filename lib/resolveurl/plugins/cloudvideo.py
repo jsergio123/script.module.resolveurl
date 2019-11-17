512e1a44cab4bc3c557b12de39872f07f37a2fd7
kingul <4146550+kingul@users.noreply.github.com>
2019-11-17 15:16:37 +0200

    Fix Cloudvideo

--- lib/resolveurl/plugins/cloudvideo.py
+++ lib/resolveurl/plugins/cloudvideo.py
@@ -1,9 +1,6 @@
 """
-    OVERALL CREDIT TO:
-        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
-
-    resolveurl XBMC Addon
-    Copyright (C) 2018 jsergio
+    Kodi resolveurl plugin
+    Copyright (C) 2019  script.module.resolveurl
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,9 +15,29 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from __resolve_generic__ import ResolveGeneric
+
+from lib import helpers
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 
 
-class CloudVideoResolver(ResolveGeneric):
+class CloudVideoResolver(ResolveUrl):
     name = 'cloudvideo'
     domains = ['cloudvideo.tv']
+    pattern = '(?://|\.)(cloudvideo\.tv)/(?:emb.html\?)?([0-9a-zA-Z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):       
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html, patterns=[r'''source\s*(?:file|src)=["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?'''], generic_patterns=False)
+        if sources:
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError('Video cannot be located.')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
