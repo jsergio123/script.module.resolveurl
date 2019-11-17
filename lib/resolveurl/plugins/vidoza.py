25ebd7c3eab2a2d341163d4d09b52f142e80ac61
kingul <4146550+kingul@users.noreply.github.com>
2019-11-17 15:18:28 +0200

    Fix Vidoza

--- lib/resolveurl/plugins/vidoza.py
+++ lib/resolveurl/plugins/vidoza.py
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from lib import helpers
+from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 
 
@@ -24,8 +25,18 @@ class VidozaResolver(ResolveUrl):
     domains = ['vidoza.net']
     pattern = '(?://|\.)(vidoza\.net)/(?:embed-)?([0-9a-zA-Z]+)'
 
-    def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id), patterns=['''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*res\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)'''], generic_patterns=False).replace(' ', '%20')
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):       
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html, patterns=[r'''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*res\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)'''], generic_patterns=False)
+        if sources:
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        raise ResolverError('Video cannot be located.')
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
