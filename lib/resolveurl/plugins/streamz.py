860ca172e7b373e12480d472718126464aa2a69b
Twilight0 <twilight@freemail.gr>
2020-03-17 13:12:08 +0200

    Fix streamz

--- lib/resolveurl/plugins/streamz.py
+++ lib/resolveurl/plugins/streamz.py
@@ -29,15 +29,22 @@ class StreamzResolver(ResolveUrl):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
+
         web_url = self.get_url(host, media_id)
         headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
-        if 'File not found, sorry!' not in html:
-            sources = helpers.scrape_sources(html)
-            if sources:
-                return helpers.pick_source(sources) + helpers.append_headers(headers)
 
-        raise ResolverError("Video not found")
+        html += helpers.get_packed_data(html)
+        sources = helpers.scrape_sources(html)
+
+        if sources:
+
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+
+        else:
+
+            raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
+
         return self._default_get_url(host, media_id, template='https://{host}/{media_id}')
