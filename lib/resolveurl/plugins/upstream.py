18210c64b00a9cbbd68a2f087caee161e7760038
mbebe <drazah@o2.pl>
2020-03-24 17:54:38 +0100

    update upstream

--- lib/resolveurl/plugins/upstream.py
+++ lib/resolveurl/plugins/upstream.py
@@ -17,6 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __resolve_generic__ import ResolveGeneric
+from resolveurl import common
 from lib import helpers
 
 
@@ -24,11 +25,17 @@ class UpStreamResolver(ResolveGeneric):
     name = "upstream.to"
     domains = ['upstream.to']
     pattern = r'(?://|\.)(upstream\.to)/(?:embed-)?([0-9a-zA-Z]+)'
-
+    def __init__(self):
+        self.net = common.Net()
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''file:\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False)
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        sources = helpers.scrape_sources(html)
+        if sources:
+            return helpers.pick_source(sources)+ helpers.append_headers(headers) +'&auth=SSL/TLS&verifypeer=false'
+                
+        raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
