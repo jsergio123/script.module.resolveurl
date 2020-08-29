767a2c1585a6c5368c21b0757086061a17840c8e
mbebe <drazah@o2.pl>
2020-08-29 14:58:06 +0200

    Update gounlimited.py

--- lib/resolveurl/plugins/gounlimited.py
+++ lib/resolveurl/plugins/gounlimited.py
@@ -15,19 +15,27 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
+import re
+from resolveurl.resolver import ResolveUrl, ResolverError
+from resolveurl import common
+from resolveurl.plugins.lib import jsunpack
 from resolveurl.plugins.lib import helpers
 
-
-class GoUnlimitedResolver(ResolveGeneric):
+class GoUnlimitedResolver(ResolveUrl):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = r'(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''src:\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False)
-
+	
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        r = re.search(r"text/javascript'>(eval.*?)\s*</script>", html, re.DOTALL)
+        if r:
+            html = jsunpack.unpack(r.group(1))
+            sources = helpers.scrape_sources(html)
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
