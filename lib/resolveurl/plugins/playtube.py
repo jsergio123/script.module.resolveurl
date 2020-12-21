1de1f94dfe5ec518938b8bc684d92f8c0d029cf2
anis3 <anis3.n@protonmail.com>
2020-12-21 05:50:01 +0000

    Fix Playtube resolver

--- lib/resolveurl/plugins/playtube.py
+++ lib/resolveurl/plugins/playtube.py
@@ -16,22 +16,32 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import re
+from resolveurl import common
+from resolveurl.resolver import ResolveUrl, ResolverError
 from resolveurl.plugins.lib import helpers
-from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 
 
-class PlayTubeResolver(ResolveGeneric):
+class PlayTubeResolver(ResolveUrl):
     name = "playtube"
     domains = ["playtube.ws"]
     pattern = r'(?://|\.)(playtube\.ws)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id),
-                                     patterns=[r'''sources\s*:\s*\["(?P<url>[^"]+)''',
-                                               r'''file:\s*"(?P<url>[^"]+)'''],
-                                     generic_patterns=False,
-                                     result_blacklist=['dl', '.srt', '.vtt'],
-                                     referer=False)
+        web_url = self.get_url(host, media_id)
+        headers = {'Origin': 'https://{}'.format(host),
+                   'Referer': 'https://{}/'.format(host),
+                   'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        if '(p,a,c,k,e,d)' in html:
+            html = helpers.get_packed_data(html)
+        r = re.search(r'src:\s*"([^"]+)', html)
+
+        if r:
+            headers = {'Origin': 'https://playtube.ws', 'Referer': 'https://playtube.ws/'}
+            return r.group(1) + helpers.append_headers(headers)
+
+        raise ResolverError("Video not found")
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
